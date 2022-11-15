f90f4390ba00e6e174bdfdd18ad1b9ded1f44b50
abhiyankhanal <abhiyan5588@gmail.com>
2022-11-15 16:24:06 +0545

    added question as comment

--- varMethods.php
+++ varMethods.php
@@ -1,4 +1,9 @@
 <?php
+//1. Write a code, using listed PHP functions, with example 
+// a. is_int() 
+// b. is_numeric() 
+// c. is_integer() 
+
 //test variables here
 $var = array(1,"4","five",32.5);
 for($i=0; $i < count($var); $i++){
@@ -7,15 +12,18 @@ for($i=0; $i < count($var); $i++){
  } else {
     echo "The method is_int returned false for $var[$i] \n";
 }
+
   if(is_integer($var[$i])==1){
     echo "The method is_integer returned true for $var[$i] \n";
  } else {
     echo "The method is_integer returned false for $var[$i] \n";
 }
+
   if(is_numeric($var[$i])==1){
     echo "The method is_numeric returned true for $var[$i] \n";
  } else {
     echo "The method is_numeric returned false for $var[$i] \n";
 }
+
 }
 ?>
