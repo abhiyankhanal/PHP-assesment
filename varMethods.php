b9d1c39f4e423f04918df07c530c8b9547c90b32
abhiyankhanal <abhiyan5588@gmail.com>
2022-11-15 16:13:35 +0545

    first solution

---
+++ varMethods.php
@@ -0,0 +1,21 @@
+<?php
+//test variables here
+$var = array(1,"4","five",32.5);
+for($i=0; $i < count($var); $i++){
+  if(is_int($var[$i])==1){
+    echo "The method is_int returned true for $var[$i] \n";
+ } else {
+    echo "The method is_int returned false for $var[$i] \n";
+}
+  if(is_integer($var[$i])==1){
+    echo "The method is_integer returned true for $var[$i] \n";
+ } else {
+    echo "The method is_integer returned false for $var[$i] \n";
+}
+  if(is_numeric($var[$i])==1){
+    echo "The method is_numeric returned true for $var[$i] \n";
+ } else {
+    echo "The method is_numeric returned false for $var[$i] \n";
+}
+}
+?>
